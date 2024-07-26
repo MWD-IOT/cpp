87f7a6e156ff57e7df754d213364601ba1b3d8bd
moweida <2594041017@qq.com>
2024-07-26 15:45:44 +0800

    new file:   CppPP_Demos/2/2-1/myfirst.cpp 	new file:   CppPP_Demos/2/2-2/carrot.cpp

---
+++ CppPP_Demos/2/2-1/myfirst.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "come up and C++ me some time.";
+    cout <<endl;
+    cout << "You won't forget it!" << endl;
+
+    return 0;
+}
+
---
+++ CppPP_Demos/2/2-2/carrot.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int carrot;
+
+    carrot = 25;
+    cout << "I have ";
+    cout << carrot;
+    cout << " carrot.";
+    cout << endl;
+    carrot = carrot - 1;
+    cout << "Crunch, crunch. Now I have " << carrot << " carrot." << endl;
+
+    return 0;
+}
+
+
