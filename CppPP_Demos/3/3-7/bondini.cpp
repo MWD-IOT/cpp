a7072ea2c1a29591afd988b283ec1386f1c5e20a
moweida <2594041017@qq.com>
2024-08-05 17:40:40 +0800

    new file:   CppPP_Demos/3/3-3/hexoct.cpp 	new file:   CppPP_Demos/3/3-4/hexoct2.cpp 	new file:   CppPP_Demos/3/3-5/chartype.cpp 	new file:   CppPP_Demos/3/3-6/morechar.cpp 	new file:   CppPP_Demos/3/3-7/bondini.cpp

---
+++ CppPP_Demos/3/3-7/bondini.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+
+    cout << "\aOperation \"HyperHyper\" is now activated!\n";
+    cout << "Enter your agent code:_______\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou entered " << code << "...\n";
+    cout << "\aCode verified! Processd with Plan Z3!\n";
+
+    return 0;
+}
